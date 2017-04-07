8ccfe2d09cda68987e1b3578100f02e38a91ccce
Jonah K <jonahkalkman@gmail.com>
2017-04-07 11:31:51 +0200

    U got connected
    #theplug

--- Project3/Database/Program.cs
+++ Project3/Database/Program.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Database
 {
+
     static class Program
     {
+        class Sample
+        {
+            static void Main(string[] args)
+            {
+                // Connect to a PostgreSQL database
+                NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres; " +
+                   "Password=pwd;Database=postgres;");
+                conn.Open();
+
+                // Define a query returning a single row result set
+                NpgsqlCommand command = new NpgsqlCommand("SELECT COUNT(*) FROM cities", conn);
+
+                // Execute the query and obtain the value of the first column of the first row
+                Int64 count = (Int64)command.ExecuteScalar();
+
+                Console.Write("{0}\n", count);
+                conn.Close();
+            }
+        }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
