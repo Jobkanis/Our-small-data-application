4f48cf66810cf1d4de0be2641f1542f3d3498dca
Jonah K <jonahkalkman@gmail.com>
2017-04-07 11:36:02 +0200

    dwadwdw

--- Project3/Database/Program.cs
+++ Project3/Database/Program.cs
@@ -12,32 +12,30 @@ namespace Database
     {
         class Sample
         {
-            static void Main(string[] args)
-            {
-                // Connect to a PostgreSQL database
-                NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres; " +
-                   "Password=pwd;Database=postgres;");
-                conn.Open();
-
-                // Define a query returning a single row result set
-                NpgsqlCommand command = new NpgsqlCommand("SELECT COUNT(*) FROM cities", conn);
-
-                // Execute the query and obtain the value of the first column of the first row
-                Int64 count = (Int64)command.ExecuteScalar();
-
-                Console.Write("{0}\n", count);
-                conn.Close();
-            }
+            
+                
+            
         }
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // Connect to a PostgreSQL database
+            NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres; " +
+               "Password=pwd;Database=postgres;");
+            conn.Open();
+
+            // Define a query returning a single row result set
+            NpgsqlCommand command = new NpgsqlCommand("SELECT COUNT(*) FROM cities", conn);
+
+            // Execute the query and obtain the value of the first column of the first row
+            Int64 count = (Int64)command.ExecuteScalar();
+
+            Console.Write("{0}\n", count);
+            conn.Close();
         }
     }
 }
