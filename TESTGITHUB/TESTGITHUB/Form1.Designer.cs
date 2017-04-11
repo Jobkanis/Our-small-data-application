2d61477fc7e3d2bfcca8d6fd7821b1c05f18c391
Dionykn <dionklein12@hotmail.com>
2017-04-11 16:27:26 +0200

    Fresh start
    hueheuehuehue

--- TESTGITHUB/TESTGITHUB/Form1.Designer.cs
+++ TESTGITHUB/TESTGITHUB/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            //this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -50,7 +50,7 @@
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            //this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- TESTGITHUB/TESTGITHUB/Form1.cs
+++ TESTGITHUB/TESTGITHUB/Form1.cs
@@ -7,24 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace TESTGITHUB
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
+
+            // Specify connection options and open an connection
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;User Id=postgres;" +
+                                    "Password=DionKlein98;Database=project3;");
+            conn.Open();
+
+
+            // Define a query
+            NpgsqlCommand cmd = new NpgsqlCommand("select straat,plaats from fietsdiefstal", conn);
+
+            // Execute the query and obtain a result set
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            List<string> straat = new List<string>();
+            List<string> plaats = new List<string>();
+            // Output rows
+            while (dr.Read())
+            {
+                //Console.Write("{0}\t{1} \n", dr[0], dr[1]);
+
+                straat.Add(dr.GetValue(0).ToString());
+                plaats.Add(dr.GetValue(1).ToString());
+            }
+
+
+
+            Console.WriteLine(plaats.Count());
+            Console.WriteLine(straat.Count());
+            Console.WriteLine(straat[3] + " te " + plaats[3]);
+
+
+
+            conn.Close();
+
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
