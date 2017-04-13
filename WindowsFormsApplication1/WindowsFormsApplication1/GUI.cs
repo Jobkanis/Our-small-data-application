62839720eb5504b6730d8176a83eada9e9fc0493
Jonah K <jonahkalkman@gmail.com>
2017-04-13 11:42:22 +0200

    commit

--- WindowsFormsApplication1/WindowsFormsApplication1/GUI.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/GUI.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        
 
         public Form1()
         {
+            string databaseplace = "C:\\Users\\Jonah Kalkman\\Desktop\\Project3\\WindowsFormsApplication1\\WindowsFormsApplication1\\Official_Database.mdf";
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename="+ databaseplace+ ";Integrated Security=True");
+            SqlCommand cmd;
+            SqlDataReader dr;
+            cmd = new SqlCommand("select Begintijd from fietsdiefstal", con);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            Console.WriteLine("pen15 -> " + dr.HasRows);
+            int counter = 0;
+            
+            while (dr.Read())
+            {
+                //get rows
+                counter++;
+            }
+            Console.WriteLine(counter);
+            con.Close();
             InitializeComponent();
         }
 
