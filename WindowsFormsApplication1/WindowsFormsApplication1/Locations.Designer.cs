06d0ddb57ed16ce4ab8e1ba1a0a72626f5e44495
RobinBreedveld <robinbreedveld@hotmail.com>
2017-04-18 14:42:03 +0200

    'amAZIN' LOCATIONS

--- WindowsFormsApplication1/WindowsFormsApplication1/Locations.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Locations.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button_District1 = new System.Windows.Forms.Button();
             this.button_District12 = new System.Windows.Forms.Button();
@@ -37,7 +40,10 @@
             this.button_District3 = new System.Windows.Forms.Button();
             this.button_District6 = new System.Windows.Forms.Button();
             this.button_District2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.DrawGraph = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -51,7 +57,7 @@
             this.panel1.Controls.Add(this.button_District3);
             this.panel1.Controls.Add(this.button_District6);
             this.panel1.Controls.Add(this.button_District2);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(54, 89);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(289, 283);
             this.panel1.TabIndex = 1;
@@ -139,18 +145,61 @@
             this.button_District2.UseVisualStyleBackColor = true;
             this.button_District2.Click += new System.EventHandler(this.button_District2_Click);
             // 
+            // chart1
+            // 
+            this.chart1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.chart1.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Top;
+            chartArea2.AxisX.IsStartedFromZero = false;
+            chartArea2.AxisX.Maximum = 25D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisX.ScaleBreakStyle.MaxNumberOfBreaks = 1;
+            chartArea2.AxisX.ScaleView.MinSizeType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea2.AxisX.ScaleView.SizeType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.chart1.Enabled = false;
+            this.chart1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.chart1.Location = new System.Drawing.Point(407, 89);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series3.ChartArea = "ChartArea1";
+            series3.Name = "Fietsdiefstal";
+            series4.ChartArea = "ChartArea1";
+            series4.Name = "Straatroof";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(510, 283);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // DrawGraph
+            // 
+            this.DrawGraph.Location = new System.Drawing.Point(83, 378);
+            this.DrawGraph.Name = "DrawGraph";
+            this.DrawGraph.Size = new System.Drawing.Size(216, 25);
+            this.DrawGraph.TabIndex = 11;
+            this.DrawGraph.Text = "Update Graph";
+            this.DrawGraph.UseVisualStyleBackColor = true;
+            this.DrawGraph.Click += new System.EventHandler(this.DrawGraph_Click);
+            // 
             // Locations
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(705, 323);
+            this.ClientSize = new System.Drawing.Size(1212, 551);
+            this.Controls.Add(this.DrawGraph);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Locations";
             this.Text = "Locations";
             this.Load += new System.EventHandler(this.Locations_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -165,5 +214,7 @@
         private System.Windows.Forms.Button button_District10;
         private System.Windows.Forms.Button button_District9;
         private System.Windows.Forms.Button button_District1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button DrawGraph;
     }
 }
