4e63f9a24f3120a23c9edc006aa6bcf8305a58b0
Dionykn <dionklein12@hotmail.com>
2017-04-19 10:11:03 +0200

    same

--- WindowsFormsApplication1/WindowsFormsApplication1/JobsGraph.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/JobsGraph.cs
@@ -63,6 +63,7 @@ namespace WindowsFormsApplication1
             // Job : string databaseplace = "C:\\Users\\jobka\\Documents\\GitHub\\Project3\\WindowsFormsApplication1\\WindowsFormsApplication1\\Official_Database.mdf";
             // Oguzhan :string databaseplace = "C:\\Users\\Oguzhan\\Documents\\GitHub\\Project3\\WindowsFormsApplication1\\WindowsFormsApplication1\\Official_Database.mdf";
             // Robin : string databaseplace = "C:\\Users\\robin\\Documents\\GitHub\\Project3\\WindowsFormsApplication1\\WindowsFormsApplication1\\Official_Database.mdf";
+            // Dion : string databaseplace = "C:\\Users\\Dionykn\\Documents\\GitHub\\Project3\\WindowsFormsApplication1\\WindowsFormsApplication1\\Official_Database.mdf";
 
             string databaseplace = "C:\\Users\\robin\\Documents\\GitHub\\Project3\\WindowsFormsApplication1\\WindowsFormsApplication1\\Official_Database.mdf"; //Database location on computer
 
